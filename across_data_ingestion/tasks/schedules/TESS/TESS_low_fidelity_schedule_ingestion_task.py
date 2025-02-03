93f798533902dbfe3e7d3bd725e184487c74e3e6
Kirill Vorobyev <35638949+Kirill-Vorobyev@users.noreply.github.com>
2025-02-03 17:10:19 -0500

    refactor(review): abstract out creation of observations into methods

--- across_data_ingestion/tasks/schedules/TESS/TESS_low_fidelity_schedule_ingestion_task.py
+++ across_data_ingestion/tasks/schedules/TESS/TESS_low_fidelity_schedule_ingestion_task.py
@@ -8,6 +8,55 @@ from fastapi_utils.tasks import repeat_every
 logger = logging.getLogger("uvicorn.error")
 
 SECONDS_IN_A_WEEK = 60 * 60 * 24 * 7
+TESS_CENTRAL_WAVELENGTH = 7865
+TESS_BANDWIDTH = 4000
+TESS_TELESCOPE_ID = "some-tess-telescope-uuid"
+
+
+def create_orbit_observation(sector, ra, dec, roll, i, orbit, obs_start, obs_end):
+    # orbit start/end times are strings (non isot)
+    obs_start_str = str.replace(obs_start, " ", "T")
+    obs_end_str = str.replace(obs_end, " ", "T")
+    obs_start_at = Time(obs_start_str, format="isot")
+    obs_end_at = Time(obs_end_str, format="isot")
+    exposure_time = obs_end_at - obs_start_at
+
+    return {
+        "telescope_id": TESS_TELESCOPE_ID,
+        "object_name": f"TESS_sector_{sector}_obs_{i}_orbit_{int(orbit)}",
+        "pointing_position": {"ra": ra, "dec": dec},
+        "pointing_angle": roll,
+        "date_range": {"begin": obs_start_str, "end": obs_end_str},
+        "exposure_time": int(exposure_time.to(u.second).value),
+        "status": "planned",
+        "type": "imaging",
+        "bandpass": {
+            "central_wavelength": TESS_CENTRAL_WAVELENGTH,
+            "bandwidth": TESS_BANDWIDTH,
+            "filter_name": "TESS",
+        },
+    }
+
+
+def create_placeholder_observation(
+    schedule, sector, ra, dec, roll, sched_start_at, sched_end_at
+):
+    exposure_time = sched_end_at - sched_start_at
+    return {
+        "telescope_id": TESS_TELESCOPE_ID,
+        "object_name": f"TESS_sector_{sector}_placeholder",
+        "pointing_position": {"ra": ra, "dec": dec},
+        "pointing_angle": roll,
+        "date_range": schedule["date_range"],
+        "exposure_time": int(exposure_time.to(u.second).value),
+        "status": "planned",
+        "type": "imaging",
+        "bandpass": {
+            "central_wavelength": TESS_CENTRAL_WAVELENGTH,
+            "bandwidth": TESS_BANDWIDTH,
+            "filter_name": "TESS",
+        },
+    }
 
 
 def ingest():
@@ -20,41 +69,43 @@ def ingest():
     tess_orbit_times_file = "https://tess.mit.edu/public/files/TESS_orbit_times.csv"
     orbit_observations_df = pd.read_csv(tess_orbit_times_file)
 
-    sector_schedules = zip(
-        sector_pointings_df["Sector"],
-        sector_pointings_df["RA"],
-        sector_pointings_df["Dec"],
-        sector_pointings_df["Roll"],
-        sector_pointings_df["Start"],
-        sector_pointings_df["End"],
+    sector_schedules = list(
+        zip(
+            sector_pointings_df["Sector"],
+            sector_pointings_df["RA"],
+            sector_pointings_df["Dec"],
+            sector_pointings_df["Roll"],
+            sector_pointings_df["Start"],
+            sector_pointings_df["End"],
+        )
     )
 
     # GET Telecope by name not yet implemented in across-server
     tess_telescope_info = {"id": "some-tess-telescope-uuid"}
 
-    # Initialize List of Schedules to append to
+    # Initialize List of Schedules to append
     schedules = []
 
+    # Iterate pointings file by row
     for sector, ra, dec, roll, sector_start_date, sector_end_date in sector_schedules:
+        # Create base schedule from  each pointings file row and set date range
         schedule = {
             "name": f"TESS_sector_{sector}",
             "telescope_id": tess_telescope_info["id"],
             "status": "planned",
             "fidelity": "low",
         }
-
         sched_start_at = Time(sector_start_date, format="jd")
         sched_end_at = Time(sector_end_date, format="jd")
-
         schedule["date_range"] = {
             "begin": str(sched_start_at.to_datetime()),
             "end": str(sched_end_at.to_datetime()),
         }
 
+        # Find planned orbits from TESS_orbit_times.csv for current sector from pointings row
         orbit_observations = orbit_observations_df.loc[
             orbit_observations_df["Sector"] == str(sector)
         ]
-
         sector_orbit_observations = list(
             zip(
                 orbit_observations["Orbit"],
@@ -63,55 +114,23 @@ def ingest():
             )
         )
 
-        # Initialize list of observations to append to
+        # Initialize list of observations to append
         observations = []
-
-        if len(sector_orbit_observations) == 0:
-            # Create one overarching placeholder observation from low fidelity sector pointing schedule when no orbit times exist
-            # The pointings file contains a location and can be treated as one observation within the schedule when no orbits are yet planned
-            exposure_time = sched_end_at - sched_start_at
-            placeholder_observation = {
-                "telescope_id": tess_telescope_info["id"],
-                "object_name": f"TESS_sector_{sector}_placeholder",
-                "pointing_position": {"ra": ra, "dec": dec},
-                "pointing_angle": roll,
-                "date_range": schedule["date_range"],
-                "exposure_time": int(exposure_time.to(u.second).value),
-                "status": "planned",
-                "type": "imaging",
-                "bandpass": {
-                    "central_wavelength": 8487.01,
-                    "bandwidth": 5368.88,
-                    "filter_name": "TESS",
-                },
-            }
-            observations.append(placeholder_observation)
-        else:
+        # When TESS_orbit_times.csv has planned orbits for current sector
+        if len(sector_orbit_observations):
+            # Iterate and add orbits as observations
             for i, (orbit, obs_start, obs_end) in enumerate(sector_orbit_observations):
-                # orbit start/end times are strings (non isot)
-                obs_start_str = str.replace(obs_start, " ", "T")
-                obs_end_str = str.replace(obs_end, " ", "T")
-                obs_start_at = Time(obs_start_str, format="isot")
-                obs_end_at = Time(obs_end_str, format="isot")
-                exposure_time = obs_end_at - obs_start_at
-
-                observation = {
-                    "telescope_id": tess_telescope_info["id"],
-                    "object_name": f"TESS_sector_{sector}_obs_{i}_orbit_{int(orbit)}",
-                    "pointing_position": {"ra": ra, "dec": dec},
-                    "pointing_angle": roll,
-                    "date_range": {"begin": obs_start_str, "end": obs_end_str},
-                    "exposure_time": int(exposure_time.to(u.second).value),
-                    "status": "planned",
-                    "type": "imaging",
-                    "bandpass": {
-                        "central_wavelength": 8487.01,
-                        "bandwidth": 5368.88,
-                        "filter_name": "TESS",
-                    },
-                }
-
+                observation = create_orbit_observation(
+                    sector, ra, dec, roll, i, orbit, obs_start, obs_end
+                )
                 observations.append(observation)
+        else:
+            # Create one placeholder observation from low fidelity sector pointing file when no orbit times exist
+            # The pointings file contains a location and can be treated as one observation within the schedule when no orbits are yet planned
+            placeholder_observation = create_placeholder_observation(
+                schedule, sector, ra, dec, roll, sched_start_at, sched_end_at
+            )
+            observations.append(placeholder_observation)
 
         schedule["observations"] = observations
         schedules.append(schedule)
@@ -122,7 +141,7 @@ def ingest():
 
 
 @repeat_every(seconds=SECONDS_IN_A_WEEK)  # Weekly
-async def TESS_low_fidelity_schedule_ingestion_task():
+def TESS_low_fidelity_schedule_ingestion_task():
     current_time = Time.now()
 
     try:
@@ -131,4 +150,4 @@ async def TESS_low_fidelity_schedule_ingestion_task():
         # Surface the error through logging, if we do not catch everything and log, the errors get voided
         logger.error(f"{__name__} encountered an error {E}")
 
-    logger.info(f"{__name__} ran successfully at {current_time}")
+    logger.info(f"{__name__} ran at {current_time}")
