215cb2949485e8c1414c6740543fde586fecfc87
Kirill Vorobyev <35638949+Kirill-Vorobyev@users.noreply.github.com>
2025-02-05 13:17:52 -0500

    test(TESS): refactor into two data transformation tests to independently test branches, add logging tests

--- tests/tasks/schedules/TESS/TESS_low_fidelity_schedule_ingestion_task_test.py
+++ tests/tasks/schedules/TESS/TESS_low_fidelity_schedule_ingestion_task_test.py
@@ -11,39 +11,91 @@ from across_data_ingestion.tasks.schedules.TESS import (
 
 
 class TestTESSLowFidelityScheduleIngestionTask(TestCase):
-    def create_mock_relative_filepaths(self):
+    def create_mock_placeholder_obs_relative_filepaths(self):
         current_dir = os.path.dirname(__file__)
-        mock_orbit_times_file = os.path.join(
-            current_dir, "mocks", "TESS_orbit_times.csv"
+        # files for testing placeholder observations without orbits observations data
+        mock_orbit_times_placeholder_obs_file = os.path.join(
+            current_dir, "mocks", "placeholder_observations", "TESS_orbit_times.csv"
         )
-        mock_pointings_file = os.path.join(current_dir, "mocks", "TESS_pointings.csv")
-        across_schedule_output = os.path.join(
-            current_dir, "output", "ACROSS_schedule_output.json"
+        mock_pointings_placeholder_obs_file = os.path.join(
+            current_dir, "mocks", "placeholder_observations", "TESS_pointings.csv"
+        )
+        across_schedule_placeholder_obs_output = os.path.join(
+            current_dir,
+            "mocks",
+            "placeholder_observations",
+            "ACROSS_schedule_output.json",
         )
 
-        return mock_orbit_times_file, mock_pointings_file, across_schedule_output
+        return (
+            mock_orbit_times_placeholder_obs_file,
+            mock_pointings_placeholder_obs_file,
+            across_schedule_placeholder_obs_output,
+        )
+
+    def create_mock_orbit_obs_relative_filepaths(self):
+        current_dir = os.path.dirname(__file__)
+        # files for testing ingestion of orbits observations for each schedule
+        mock_orbit_times_orbits_obs_file = os.path.join(
+            current_dir, "mocks", "orbit_observations", "TESS_orbit_times.csv"
+        )
+        mock_pointings_orbits_obs_file = os.path.join(
+            current_dir, "mocks", "orbit_observations", "TESS_pointings.csv"
+        )
+        across_schedule_orbits_obs_output = os.path.join(
+            current_dir, "mocks", "orbit_observations", "ACROSS_schedule_output.json"
+        )
+
+        return (
+            mock_orbit_times_orbits_obs_file,
+            mock_pointings_orbits_obs_file,
+            across_schedule_orbits_obs_output,
+        )
 
     @pytest.mark.asyncio
-    def test_should_generate_across_schedules(self):
-        """Should generate schedules sucessfully"""
-        mock_orbit_times_file, mock_pointings_file, across_schedule_output = (
-            self.create_mock_relative_filepaths()
-        )
+    def test_should_generate_across_schedules_with_placeholder_observations(self):
+        """Should generate ACROSS schedules with placeholder observations"""
+        (
+            mock_orbit_times_placeholder_obs_file,
+            mock_pointings_placeholder_obs_file,
+            across_schedule_placeholder_obs_output,
+        ) = self.create_mock_placeholder_obs_relative_filepaths()
 
         with patch(
             "across_data_ingestion.tasks.schedules.TESS.TESS_low_fidelity_schedule_ingestion_task.TESS_POINTINGS_FILE",
-            new=mock_pointings_file,
+            new=mock_pointings_placeholder_obs_file,
         ), patch(
             "across_data_ingestion.tasks.schedules.TESS.TESS_low_fidelity_schedule_ingestion_task.TESS_ORBIT_TIMES_FILE",
-            new=mock_orbit_times_file,
+            new=mock_orbit_times_placeholder_obs_file,
         ):
             schedules = TESS_low_fidelity_schedule_ingestion_task()
-            with open(across_schedule_output) as expected_output_file:
+            with open(across_schedule_placeholder_obs_output) as expected_output_file:
                 expected = json.load(expected_output_file)
                 self.assertEqual(json.dumps(schedules), json.dumps(expected))
 
     @pytest.mark.asyncio
-    def test_should_raise_file_not_found_when_not_found(self):
+    def test_should_generate_across_schedules_with_orbit_observations(self):
+        """Should generate ACROSS schedules with orbit observations"""
+        (
+            mock_orbit_times_orbits_obs_file,
+            mock_pointings_orbits_obs_file,
+            across_schedule_orbits_obs_output,
+        ) = self.create_mock_orbit_obs_relative_filepaths()
+
+        with patch(
+            "across_data_ingestion.tasks.schedules.TESS.TESS_low_fidelity_schedule_ingestion_task.TESS_POINTINGS_FILE",
+            new=mock_pointings_orbits_obs_file,
+        ), patch(
+            "across_data_ingestion.tasks.schedules.TESS.TESS_low_fidelity_schedule_ingestion_task.TESS_ORBIT_TIMES_FILE",
+            new=mock_orbit_times_orbits_obs_file,
+        ):
+            schedules = TESS_low_fidelity_schedule_ingestion_task()
+            with open(across_schedule_orbits_obs_output) as expected_output_file:
+                expected = json.load(expected_output_file)
+                self.assertEqual(json.dumps(schedules), json.dumps(expected))
+
+    @pytest.mark.asyncio
+    def test_should_log_error_when_file_not_found(self):
         """Should log an error when file is not found"""
         mock_orbit_times_file, mock_pointings_file = ("", "")
 
@@ -57,4 +109,25 @@ class TestTESSLowFidelityScheduleIngestionTask(TestCase):
             new=mock_orbit_times_file,
         ):
             TESS_low_fidelity_schedule_ingestion_task()
-            assert "error" in log_mock.error.call_args.args[0]
+            assert "encountered an error" in log_mock.error.call_args.args[0]
+
+    @pytest.mark.asyncio
+    def test_should_log_info_when_success(self):
+        """Should log info with ran at when success"""
+        (
+            mock_orbit_times_placeholder_obs_file,
+            mock_pointings_placeholder_obs_file,
+            across_schedule_placeholder_obs_output,
+        ) = self.create_mock_placeholder_obs_relative_filepaths()
+
+        with patch(
+            "across_data_ingestion.tasks.schedules.TESS.TESS_low_fidelity_schedule_ingestion_task.logger"
+        ) as log_mock, patch(
+            "across_data_ingestion.tasks.schedules.TESS.TESS_low_fidelity_schedule_ingestion_task.TESS_POINTINGS_FILE",
+            new=mock_pointings_placeholder_obs_file,
+        ), patch(
+            "across_data_ingestion.tasks.schedules.TESS.TESS_low_fidelity_schedule_ingestion_task.TESS_ORBIT_TIMES_FILE",
+            new=mock_orbit_times_placeholder_obs_file,
+        ):
+            TESS_low_fidelity_schedule_ingestion_task()
+            assert "ran at" in log_mock.info.call_args.args[0]
