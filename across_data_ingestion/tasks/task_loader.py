1f992584c20fe5d95e60904ebdd33866165f772c
swyatt7 <swyatt@email.arizona.edu>
2025-04-24 15:53:07 -0700

    ixpe schedule igestion

--- across_data_ingestion/tasks/task_loader.py
+++ across_data_ingestion/tasks/task_loader.py
@@ -2,6 +2,9 @@ from .example import example_task
 from .schedules.fermi.lat_planned import (
     entrypoint as fermi_planned_schedule_ingestion_task,
 )
+from .schedules.ixpe.low_fidelity_planned import (
+    entrypoint as ixpe_low_fidelity_schedule_ingestion_task,
+)
 from .schedules.tess.low_fidelity_planned import (
     entrypoint as TESS_low_fidelity_schedule_ingestion_task,
 )
@@ -14,5 +17,6 @@ async def init_tasks():
     For more information see https://fastapiutils.github.io/fastapi-utils//user-guide/repeated-tasks/
     """
     await example_task()
+    await ixpe_low_fidelity_schedule_ingestion_task()
     await TESS_low_fidelity_schedule_ingestion_task()
     await fermi_planned_schedule_ingestion_task()
