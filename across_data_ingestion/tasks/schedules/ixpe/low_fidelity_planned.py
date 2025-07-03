3e6b3384d97a97437eadc1c0821b5d29e16faa53
swyatt7 <swyatt@email.arizona.edu>
2025-07-03 15:32:04 -0700

    refactor take

--- across_data_ingestion/tasks/schedules/ixpe/low_fidelity_planned.py
+++ across_data_ingestion/tasks/schedules/ixpe/low_fidelity_planned.py
@@ -1,23 +1,20 @@
-import logging
-
 import astropy.units as u  # type: ignore[import-untyped]
 import httpx
 import pandas as pd
+import structlog
 from astropy.time import Time  # type: ignore[import-untyped]
 from bs4 import BeautifulSoup  # type: ignore[import-untyped]
 from fastapi_utils.tasks import repeat_every
 
+from ....core.constants import SECONDS_IN_A_WEEK
 from ....util import across_api
+from ..types import AcrossObservation, AcrossSchedule
 
-# from ..types import AcrossObservation, AcrossSchedule
+logger: structlog.stdlib.BoundLogger = structlog.get_logger()
 
-logger = logging.getLogger("uvicorn.error")
-
-SECONDS_IN_A_WEEK = 60 * 60 * 24 * 7
 IXPE_LTP_URL = "https://ixpe.msfc.nasa.gov/for_scientists/ltp.html"
 
-IXPE_MIN_ENERGY = 2.0  # keV
-IXPE_MAX_ENERGY = 8.0  # keV
+IXPE_BANDPASS = {"min": 2.0, "max": 12.0, "unit": "keV", "filter_name": "NICER XTI"}
 
 
 def query_ixpe_schedule(url) -> pd.DataFrame | None:
@@ -34,32 +31,45 @@ def query_ixpe_schedule(url) -> pd.DataFrame | None:
         schedule_data = []
 
         if schedule_table:
-            rows = schedule_table.find_all("tr")
+            rows = schedule_table.find_all("tr")  # type: ignore
             for row in rows:
                 columns = row.find_all("td")
                 schedule_data.append([col.get_text(strip=True) for col in columns])
 
-        header = schedule_data.pop(0)
-        return pd.DataFrame(schedule_data, columns=header)
+            # Convert to a pandas dataframe with header information
+            header = schedule_data.pop(0)
+            ixpe_df = pd.DataFrame(schedule_data, columns=header)
 
-    except httpx._exceptions.RequestError as e:
-        print(f"Error fetching the webpage: {e}")
+            # TODO: delete
+            import json
+
+            df_json = ixpe_df.to_json()
+            pw_to_mock_dump = "/Users/sdwyatt/Work/git-clones/across-data-ingestion/tests/tasks/schedules/ixpe/mocks/mock_ixpe_query.py"
+            with open(pw_to_mock_dump, "w") as f:
+                json.dump(df_json, f, indent=2)
+            ###
+
+            return ixpe_df
+
+        return None
+
+    except Exception:
         return None
 
 
 def ixpe_schedule(
     telescope_id: str, data: pd.DataFrame, status: str, fidelity: str
-):  # -> AcrossSchedule | dict:
+) -> AcrossSchedule | dict:
     """
-    Creates a NICER schedule from the provided data.
+    Creates a IXPE schedule from the provided data.
     """
 
-    begin = Time(f"{min(data["Start"])}", format="isot").isot
-    end = Time(f"{max(data["Stop"])}", format="isot").isot
+    begin = Time(f"{min(data["Start"])}", format="iso").isot
+    end = Time(f"{max(data["Stop"])}", format="iso").isot
 
     return {
         "telescope_id": telescope_id,
-        "name": f"nicer_obs_planned_{begin.split('T')[0]}_{end.split('T')[0]}",
+        "name": f"ixpe_ltp_{begin.split('T')[0]}_{end.split('T')[0]}",
         "date_range": {
             "begin": begin,
             "end": end,
@@ -69,93 +79,87 @@ def ixpe_schedule(
     }
 
 
-def ixpe_observation(
-    object_name, ra, dec, obs_start, obs_end, instrument_id, P_s, Pnum
-):
-    # orbit start/end times are strings (non isot)
-    obs_start_at = Time(f"{obs_start}:00:00", format="isot")
-    obs_end_at = Time(f"{obs_end}:00:00", format="isot")
+def ixpe_observation(instrument_id: str, row: dict) -> AcrossObservation:
+    """
+    Creates a IXPE observation from the provided row of data.
+    Calculates the exposure time from the End - Start
+    Sets the external_id a custom value based off of the P S and Pnum values
+    """
+    obs_start_at = Time(row["Start"], format="iso")
+    obs_end_at = Time(row["Stop"], format="iso")
 
     exposure_time = obs_end_at - obs_start_at
 
-    mock_external_id = f"ixpe_{str.replace(P_s, " ", "_")}_obs_{Pnum}"
+    external_id = f"ixpe_{str.replace(row["P S"], " ", "_")}_obs_{row['Pnum']}"
+
     return {
         "instrument_id": instrument_id,
-        "object_name": object_name,
-        "external_observation_id": mock_external_id,
-        "pointing_position": {"ra": ra, "dec": dec},
-        "pointing_angle": 0.0,
-        "date_range": {"begin": str(obs_start_at), "end": str(obs_end_at)},
-        "exposure_time": int(exposure_time.to(u.second).value),
-        "status": "planned",
-        "type": "imaging",
-        "bandpass": {
-            "min": IXPE_MIN_ENERGY,
-            "max": IXPE_MAX_ENERGY,
-            "unit": "keV",
-            "filter_name": "IXPE",
+        "object_name": f"{row["Name"]}",
+        "pointing_position": {
+            "ra": f"{row["RA"]}",
+            "dec": f"{row["Dec"]}",
         },
+        "object_position": {
+            "ra": f"{row["RA"]}",
+            "dec": f"{row["Dec"]}",
+        },
+        "date_range": {
+            "begin": obs_start_at.isot,
+            "end": obs_end_at.isot,
+        },
+        "external_observation_id": external_id,
+        "type": "imaging",
+        "status": "planned",
+        "exposure_time": int(exposure_time.to(u.second).value),
+        "bandpass": IXPE_BANDPASS,
+        "pointing_angle": 0.0,  # No Value for position angle -_-
     }
 
 
-def ingest():
+def ingest() -> AcrossSchedule | dict:
     """
     Fetches the IXPE schedule from the specified URL and returns the parsed data.
     """
+    ixpe_df = query_ixpe_schedule(IXPE_LTP_URL)
+    if ixpe_df is None:
+        logger.warn("Failed to read IXPE timeline file")
+        return {}
+
+    # IXPE doesn't give an end time for these schedules
+    # Populate them with the next observations start time.
+    # The last target won't have an end time, so fill it with its start time
+    ixpe_df["Stop"] = ixpe_df["Start"].shift(-1).fillna(ixpe_df["Start"])
+
+    # GET Telescope by name
     tess_telescope_info = across_api.telescope.get({"name": "ixpe"})[0]
     telescope_id = tess_telescope_info["id"]
     instrument_id = tess_telescope_info["instruments"][0]["id"]
-    schedule_data = query_ixpe_schedule(IXPE_LTP_URL)
 
-    schedule_observations = []
-    if schedule_data:
-        # Process the schedule data as needed
-        for i, entry in enumerate(schedule_data[1:]):  # Skip header row
-            P_s = entry[0]
-            Pnum = entry[1]
-            Name = entry[2]
-            Ra = float(entry[3])
-            Dec = float(entry[4])
-            start_time = entry[5]
+    # Initialize schedule
+    schedule = ixpe_schedule(
+        telescope_id=telescope_id, data=ixpe_df, status="planned", fidelity="low"
+    )
 
-            if i < len(schedule_data) - 1:
-                end_time = schedule_data[i + 1][5]
-            else:
-                end_time = entry[5]
+    # Transform dataframe to list of dictionaries
+    schedule_observations = ixpe_df.to_dict(orient="records")
 
-            observation = ixpe_observation(
-                object_name=Name,
-                ra=Ra,
-                dec=Dec,
-                obs_start=start_time,
-                obs_end=end_time,
-                instrument_id=instrument_id,
-                P_s=P_s,
-                Pnum=Pnum,
-            )
-            schedule_observations.append(observation)
+    # Transform observations
+    schedule["observations"] = [
+        ixpe_observation(instrument_id, row) for row in schedule_observations
+    ]
 
-        schedule_start = schedule_observations[0]["date_range"]["begin"]
-        schedule_end = schedule_observations[-1]["date_range"]["end"]
+    # Post schedule
+    across_api.schedule.post(dict(schedule))
 
-        schedule = {
-            "name": "IXPE_LTP",
-            "telescope_id": telescope_id,
-            "status": "planned",
-            "fidelity": "low",
-        }
+    # TODO: delete
+    import json
 
-        sched_start_at = Time(schedule_start, format="isot").isot
-        sched_end_at = Time(schedule_end, format="isot").isot
+    pw_to_mock_dump = "/Users/sdwyatt/Work/git-clones/across-data-ingestion/tests/tasks/schedules/ixpe/mocks/ixpe_across_schedule.py"
+    with open(pw_to_mock_dump, "w") as f:
+        json.dump(schedule, f, indent=2)
+    ###
 
-        schedule["date_range"] = {
-            "begin": str(sched_start_at.to_datetime()),
-            "end": str(sched_end_at.to_datetime()),
-        }
-        schedule["observations"] = schedule_observations
-        across_api.schedule.post(schedule)
-    else:
-        print("No schedule data found.")
+    return schedule
 
 
 @repeat_every(seconds=SECONDS_IN_A_WEEK)  # Weekly
