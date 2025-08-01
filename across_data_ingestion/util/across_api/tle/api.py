0ceb10684af20116ba23baaf2e89eefa35695224
Craig Pellegrino <42218592+crpellegrino@users.noreply.github.com>
2025-08-01 17:06:13 -0400

    feat: add task to ingest TLEs for observatories with TLE ephemerides (#52)
    * feat: add task to ingest TLEs for observatories with TLE ephemerides
    
    * chore: address pr comments

---
+++ across_data_ingestion/util/across_api/tle/api.py
@@ -0,0 +1,41 @@
+import httpx
+import structlog
+
+from ....core.config import config  # type: ignore[import-untyped]
+from ....core.exceptions import AcrossHTTPException  # type: ignore[import-untyped]
+
+logger: structlog.stdlib.BoundLogger = structlog.get_logger()
+
+
+TLE_URL: str = f"{config.across_server_url()}/tle/"
+
+
+def post(data: dict = {}) -> None:
+    """
+    Method to utilize the ACROSS web API to post a TLE
+
+    Parameters
+    -----------
+    data: dict
+        Dictionary of valid TLE information to be posted to the ACROSS Server.
+
+    -----------
+    Raises
+    -----------
+    AcrossHTTPException
+        If the return status code is not a 201 or 409
+    """
+    headers = {
+        "accept": "application/json",
+        "Authorization": f"Bearer {config.ACROSS_INGESTION_SERVICE_ACCOUNT_KEY}",
+        "Content-Type": "application/json",
+    }
+
+    response = httpx.request("POST", url=TLE_URL, json=data, headers=headers)
+
+    if response.status_code == 201:
+        return
+    elif response.status_code == 409:
+        logger.warn(f"409: {response.text}")
+    else:
+        raise AcrossHTTPException(response.status_code, response.text, {})
--- tests/util/api_test.py
+++ tests/util/api_test.py
@@ -3,7 +3,7 @@ from unittest.mock import patch
 import pytest
 
 from across_data_ingestion.core.exceptions import AcrossHTTPException
-from across_data_ingestion.util.across_api import schedule, telescope
+from across_data_ingestion.util.across_api import observatory, schedule, telescope, tle
 
 
 class mock_response:
@@ -89,3 +89,61 @@ class TestTelescopeApi:
             ):
                 with pytest.raises(AcrossHTTPException):
                     telescope.get(params={})
+
+
+class TestObservatoryApi:
+    class TestGet:
+        def test_should_be_successful_on_200(self):
+            """Should return a successful GET"""
+            response = mock_response(status_code=200, text="response text")
+
+            with patch("httpx.request", return_value=response):
+                value = observatory.get(params={})
+                assert isinstance(value, list)
+
+        def test_should_raise_exception(self):
+            """Should raise an exception on observatory get with non-200 status code"""
+            response = mock_response(status_code=400, text="response_text")
+
+            with patch("httpx.request", return_value=response), patch(
+                "logging.error", return_value=None
+            ):
+                with pytest.raises(AcrossHTTPException):
+                    observatory.get(params={})
+
+
+class TestTLEApi:
+    class TestPost:
+        def test_should_create_201(self):
+            """Should return on a 201"""
+            response = mock_response(status_code=201, text="test_tle_params")
+
+            with patch("httpx.request", return_value=response):
+                tle_post_return = tle.post(data={})
+                assert tle_post_return is None
+
+        def test_should_be_duplicate_409(self):
+            """Should log a duplicate TLE POST"""
+            response = mock_response(
+                status_code=409,
+                text="TLE with norad_id and epoch already exists",
+            )
+
+            with patch(
+                "across_data_ingestion.util.across_api.tle.api.logger"
+            ) as log_mock, patch("httpx.request", return_value=response):
+                tle.post(data={})
+                assert response.text in log_mock.warn.call_args.args[0]
+
+        def test_should_raise_exception(self):
+            """Should raise an exception in POST with non-201 or 409 status code"""
+            response = mock_response(
+                status_code=400,
+                text="TLE with norad_id and epoch already exists",
+            )
+
+            with patch("httpx.request", return_value=response), patch(
+                "logging.error", return_value=None
+            ):
+                with pytest.raises(AcrossHTTPException):
+                    tle.post(data={})
