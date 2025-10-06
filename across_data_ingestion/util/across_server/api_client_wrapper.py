c072201bd4275ff199db52d471a1d7f177c9bc29
Kirill Vorobyev <35638949+Kirill-Vorobyev@users.noreply.github.com>
2025-10-06 15:42:43 -0400

    fix: set seeded service account client credentials when running locally

--- across_data_ingestion/util/across_server/api_client_wrapper.py
+++ across_data_ingestion/util/across_server/api_client_wrapper.py
@@ -4,7 +4,14 @@ from across.sdk.v1.api_client_wrapper import ApiClientWrapper
 from ...core import config
 from .ssm_credentials import SSMCredentials
 
+sdk_config = sdk.Configuration(host=config.ACROSS_SERVER_URL)
+
+# use local dev service account
+if config.is_local():
+    sdk_config.username = config.ACROSS_SERVER_ID
+    sdk_config.password = config.ACROSS_SERVER_SECRET
+
 client = ApiClientWrapper.get_client(
-    configuration=sdk.Configuration(host=config.ACROSS_SERVER_URL),
+    configuration=sdk_config,
     creds=SSMCredentials() if not config.is_local() else None,
 )
