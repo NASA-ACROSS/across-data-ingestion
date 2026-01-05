a19f6e616981ac63ce99398e56add2a50d539c28
swyatt7 <swyatt@email.arizona.edu>
2026-01-05 15:37:51 -0700

    feat: change TESS schedule to run every tuesday at 10:12pm

--- across_data_ingestion/tasks/schedules/tess/low_fidelity_planned.py
+++ across_data_ingestion/tasks/schedules/tess/low_fidelity_planned.py
@@ -191,7 +191,7 @@ def ingest():
     return schedules
 
 
-@repeat_at(cron="12 22 * 8 *", logger=logger)
+@repeat_at(cron="12 22 * * 1", logger=logger)
 async def entrypoint():
     try:
         logger.info("Schedule ingestion started.")
